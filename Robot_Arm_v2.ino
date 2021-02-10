d6648de82f1ede39018eeb855078637ac2d05cba
Yehuda Cohen <yehudac45@gmail.com>
2021-02-10 14:25:18 -0500

    Added .ino

---
+++ Robot_Arm_v2.ino
@@ -0,0 +1,61 @@
+#include <Servo.h>
+
+//define our servos
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+int joyX =0;
+int joyY =1;
+int push=2;
+ 
+int joyVal;
+int pushVal;
+
+bool shoulder=true;
+
+void setup()
+{ 
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(9);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(shoulder==true){
+    pushVal=analogRead(push);
+    joyVal = analogRead(joyX);
+    joyVal = map (joyVal, 0, 1023, 0, 180); //servo value between 0-180
+    servo1.write(joyVal); //set the servo position according to the joystick value
+    Serial.println("X");
+    Serial.println(joyVal);
+  
+    joyVal = analogRead(joyY);
+    joyVal = map (joyVal, 0, 1023, 0, 180);
+    servo2.write(joyVal);
+    if(pushVal==0){
+      shoulder=false;
+      delay(250);
+      }
+    delay(15);
+  }
+  else{
+    pushVal=analogRead(push);
+    joyVal = analogRead(joyX);
+    joyVal = map(joyVal, 0, 1023, 0, 115);
+    servo3.write(joyVal);
+  
+    joyVal = analogRead(joyY);
+    joyVal = map (joyVal, 0, 1023, 0, 180);
+    servo4.write(joyVal);
+    if(pushVal==0){
+      shoulder=true;
+      delay(250);
+      }
+    delay(15);
+  }
+}    
