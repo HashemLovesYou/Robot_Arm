d25361e479dbcba73c04b2e55eae0942a56498cf
Yehuda Cohen <yehudac45@gmail.com>
2021-02-10 22:31:19 -0500

    finished v3

--- v3/Robot_Arm_v3.ino
+++ v3/Robot_Arm_v3.ino
@@ -35,5 +35,37 @@ void loop(){
     servo1.write(joyVal);
     Serial.println("X");
     Serial.println(joyVal);
+
+    joyVal=analogRead(joyY);
+    velocity=map(joyVal, 0, 1023, -30, 30);
+    joyVal += velocity;
+    if(joyVal>180){joyVal=180;}
+    if(joyVal<0){joyVal=0;}
+    servo2.write(joyVal);
+
+    if(pushVal==0){
+      shoulder=false;
+      delay(250);
+      }
+    delay(15);
+  }
+  else{
+    pushVal=analogRead(push);
+    joyVal=analogRead(joyX);
+    int velocity=map(joyVal, 0, 1023, -30, 30);
+    joyVal+=velocity;
+    if(joyVal>115){joyVal=115;}
+    if(joyVal<0){joyVal=0;}
+    servo3.write(joyVal);
+
+    joyVal=analogRead(joyY);
+    velocity=map(joyVal, 0, 1023, -30, 30);
+    if(joyVal>180){joyVal=180;}
+    if(joyVal<0){joyVal=0;}
+    if(pushVal==0){
+      shoulder=true;
+      delay(250);
+      }
+    delay(15);
   }
 }
